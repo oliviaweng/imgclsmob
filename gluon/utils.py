0855cecaf248493e16085cac17c914094078d918
Olivia Weng <oliviajweng@gmail.com>
2020-11-13 06:41:23 -0800

    only unfreeze weights

--- gluon/utils.py
+++ gluon/utils.py
@@ -179,10 +179,10 @@ def prepare_model(model_name,
 
     # Unfreeze non res stack 1 conv1 layer
     net._collect_params_with_prefix()['features.1.0.body.conv1.conv.weight'].grad_req = 'write'
-    net._collect_params_with_prefix()['features.1.0.body.conv1.bn.gamma'].grad_req = 'write'
-    net._collect_params_with_prefix()['features.1.0.body.conv1.bn.beta'].grad_req = 'write'
-    net._collect_params_with_prefix()['features.1.0.body.conv1.bn.running_mean'].grad_req = 'write'
-    net._collect_params_with_prefix()['features.1.0.body.conv1.bn.running_var'].grad_req = 'write'
+    # net._collect_params_with_prefix()['features.1.0.body.conv1.bn.gamma'].grad_req = 'write'
+    # net._collect_params_with_prefix()['features.1.0.body.conv1.bn.beta'].grad_req = 'write'
+    # net._collect_params_with_prefix()['features.1.0.body.conv1.bn.running_mean'].grad_req = 'write'
+    # net._collect_params_with_prefix()['features.1.0.body.conv1.bn.running_var'].grad_req = 'write'
 
     for params in net.collect_params().values():
         if params.grad_req == 'write':
