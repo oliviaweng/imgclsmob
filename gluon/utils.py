cb68aff1a747cdf11d4d59086aeb25ea0a783265
Olivia Weng <oliviajweng@gmail.com>
2020-11-13 09:18:02 -0800

    unfreeze more params in second non res stack

--- gluon/utils.py
+++ gluon/utils.py
@@ -177,12 +177,12 @@ def prepare_model(model_name,
 
     net.collect_params().setattr('grad_req', 'null')
 
-    # Unfreeze non res stack 1 conv1 layer
-    net._collect_params_with_prefix()['features.1.0.body.conv1.conv.weight'].grad_req = 'write'
-    # net._collect_params_with_prefix()['features.1.0.body.conv1.bn.gamma'].grad_req = 'write'
-    # net._collect_params_with_prefix()['features.1.0.body.conv1.bn.beta'].grad_req = 'write'
-    # net._collect_params_with_prefix()['features.1.0.body.conv1.bn.running_mean'].grad_req = 'write'
-    # net._collect_params_with_prefix()['features.1.0.body.conv1.bn.running_var'].grad_req = 'write'
+    # Unfreeze non res stack 2 - conv1 layer
+    net._collect_params_with_prefix()['features.2.0.body.conv1.conv.weight'].grad_req = 'write'
+    net._collect_params_with_prefix()['features.2.0.body.conv1.bn.gamma'].grad_req = 'write'
+    net._collect_params_with_prefix()['features.2.0.body.conv1.bn.beta'].grad_req = 'write'
+    net._collect_params_with_prefix()['features.2.0.body.conv1.bn.running_mean'].grad_req = 'write'
+    net._collect_params_with_prefix()['features.2.0.body.conv1.bn.running_var'].grad_req = 'write'
 
     for params in net.collect_params().values():
         if params.grad_req == 'write':
