b0faf1ce1532388a1fff7e089d1dfc0512ab4458
Olivia Weng <oliviajweng@gmail.com>
2020-11-19 14:14:55 -0500

    only 1 non res layer in resnet56

--- gluon/gluoncv2/models/resnet_cifar.py
+++ gluon/gluoncv2/models/resnet_cifar.py
@@ -211,7 +211,7 @@ def nonresnet56_cifar10(classes=10, **kwargs):
         Location for keeping the model parameters.
     """
     num_non_res_stk = 1
-    num_non_res_blocks = 3
+    num_non_res_blocks = 1
     return get_resnet_cifar(classes=classes, blocks=56, bottleneck=False, model_name="nonresnet56_cifar10",
     num_non_res_stk=num_non_res_stk,
     num_non_res_blocks=num_non_res_blocks, **kwargs)
