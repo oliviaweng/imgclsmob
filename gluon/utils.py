5743dec9d352278a3ba96a9e1e50d31847c7f969
Olivia Weng <oliviajweng@gmail.com>
2020-11-23 09:47:45 -0500

    3 non res blocks and unfreeze 1st stack starting at 3rd non res block

--- gluon/utils.py
+++ gluon/utils.py
@@ -170,8 +170,8 @@ def prepare_model(model_name,
 
         # Unfreeze non res stack conv1 layer
         stack = 1 # Which stack in which to unfreeze layers
-        num_layers = 9 # Num layers in the stack to unfreeze
-        start_layer = 0 # Layer at which to start unfreezing
+        num_layers = 7 # Num layers in the stack to unfreeze
+        start_layer = 2 # Layer at which to start unfreezing
         for i in range(num_layers):
             j = str(start_layer + i)
             stk = str(stack)
